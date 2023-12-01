dbc793f696ebdc5c9b45e89cb9f598ad9558b585
ftoledo <franciscojaviertoledocollante@gmail.com>
2023-11-30 23:26:12 -0300

    first commit

---
+++ src/paginas/Proyectos.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* import { useEffect } from "react";
+
+ */
+
+import Alerta from "../components/Alerta";
+import PreviewProyecto from "../components/PreviewProyecto";
+import useProyectos from "../hooks/useProyectos";
+
+const Proyectos = () => {
+  const { proyectos, alerta } = useProyectos();
+  //console.log(proyectos);
+  const { msg } = alerta;
+
+  return (
+    <>
+      <h1 className="text-4xl font-black">Proyectos</h1>
+
+      {msg && <Alerta alerta={alerta} />}
+
+      <div className="bg-white shadow mt-10 rounded-lg ">
+        {proyectos.length ? (
+          proyectos.map((proyecto) => (
+            <PreviewProyecto key={proyecto._id} proyecto={proyecto} />
+          ))
+        ) : (
+          <p className=" text-center text-gray-600 uppercase  p-5">
+            No hay proyectos aún
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Proyectos;
